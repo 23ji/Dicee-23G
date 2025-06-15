df41061eaa0b36387870405fcbaddedce04da6d4
23ji <isj4194@naver.com>
2025-06-15 20:35:09 +0900

    initial Commits

---
+++ Dicee-23G/Dicee-23G/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-23G
+//
+//  Created by 이상지 on 6/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
